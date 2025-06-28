ad08972fa496fb4ffc8fe1ab75d51664ebc1d01f
Jay Kava <jaykava1998@gmail.com>
2025-06-28 23:24:30 +0530

    test: add returns the sum if two numbers are passed separated by comma

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -10,5 +10,9 @@ RSpec.describe StringCalculator do
     it 'returns the number itself if one number is passed' do
       expect(calc.add("1")).to eq(1)
     end
+
+    it 'returns the sum if two numbers are passed separated by comma' do
+      expect(calc.add("1,5")).to eq(6)
+    end
   end
 end
