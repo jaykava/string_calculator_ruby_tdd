efa94ed4485bd91625162f7ae3c73515ece021b9
Jay Kava <jaykava1998@gmail.com>
2025-06-29 00:23:08 +0530

    refactor: use let in rspec for the new object creation for each time

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,7 +2,7 @@ require_relative '../lib/string_calculator'
 
 RSpec.describe StringCalculator do
   describe '#add' do
-    calc = StringCalculator.new
+    let(:calc) { StringCalculator.new }
     it 'returns 0 for an empty string' do
       expect(calc.add("")).to eq(0)
     end
