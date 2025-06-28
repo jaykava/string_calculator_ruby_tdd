082dadf9ba9f6d349b28c9b16d6194e07975a9ec
Jay Kava <jaykava1998@gmail.com>
2025-06-28 23:35:17 +0530

    refactor: add new parse_numbers method for seprated the logic of add() method

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,6 +2,13 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
-		numbers.split(",").map(&:to_i).sum
+		number_list = parse_numbers(numbers)
+    number_list.sum
+  end
+
+  private
+
+  def parse_numbers(numbers)
+    numbers.split(",").map(&:to_i)
   end
 end
