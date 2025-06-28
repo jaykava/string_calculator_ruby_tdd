19f088389b372a7dcca568976319b43fd7a2e638
Jay Kava <jaykava1998@gmail.com>
2025-06-28 23:21:13 +0530

    refactor: add object creation only one time for all test

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,13 +2,12 @@ require_relative '../lib/string_calculator'
 
 RSpec.describe StringCalculator do
   describe '#add' do
+    calc = StringCalculator.new
     it 'returns 0 for an empty string' do
-      calc = StringCalculator.new
       expect(calc.add("")).to eq(0)
     end
 
     it 'returns the number itself if one number is passed' do
-      calc = StringCalculator.new
       expect(calc.add("1")).to eq(1)
     end
   end
