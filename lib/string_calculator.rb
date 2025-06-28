3b54f3d0b856ff329807daf0248f82d7477a84f6
Jay Kava <jaykava1998@gmail.com>
2025-06-29 00:26:11 +0530

    feat: created new method get_called_count to tracks how many times add() was called

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,9 +1,19 @@
 class StringCalculator
+
+  def initialize
+    @call_count = 0
+  end
+
   def add(numbers)
+    @call_count += 1
     return 0 if numbers.empty?
 
 		number_list = parse_numbers(numbers)
     number_list.sum
+  end
+
+  def get_called_count
+    @call_count
   end
 
   private
